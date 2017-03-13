75ec5de927dd91828040ead550b29a222c4361a0
CaballeroFF <CaballeroFF@users.noreply.github.com>
2017-03-13 14:47:39 -0700

    dont need a clock

--- sign_ext_mux.v
+++ sign_ext_mux.v
@@ -2,12 +2,11 @@
 //mux for execution
 //////////////////////////////////////////////////////////////////////////////////
 module sign_ext_mux(
-	input wire [31:0] sign_imm, sign_ext,
-	input wire sel, clk,
+	input wire [31:0] alu_se, ls_se,
+	input wire sel,
 	output reg [31:0] sign_out
     );
 	 
-	always @ (posedge clk)
-		sign_out = sel ? sign_imm : sign_ext;
+	assign sign_out = sel ? alu_se : ls_se;
 
 endmodule
