7ea3a324e83608b296338acf4135e309e5ca0042
CaballeroFF <CaballeroFF@users.noreply.github.com>
2017-03-14 10:07:58 -0700

    just moved it inside the file

---
+++ execution/sign_extend_mux.v
@@ -0,0 +1,12 @@
+`timescale 1ns / 1ps
+//mux for execution
+//////////////////////////////////////////////////////////////////////////////////
+module sign_ext_mux(
+	input wire [31:0] alu_se, ls_se,
+	input wire sel,
+	output reg [31:0] sign_out
+    );
+	 
+	assign sign_out = sel ? alu_se : ls_se;
+
+endmodule
