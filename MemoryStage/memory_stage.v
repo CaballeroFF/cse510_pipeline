c9c213a1e6747a35d210cb3f948fce8bfc66438f
CaballeroFF <CaballeroFF@users.noreply.github.com>
2017-03-16 12:05:01 -0700

    replaced branch_op with comparator

--- MemoryStage/memory_stage.v
+++ MemoryStage/memory_stage.v
@@ -3,8 +3,9 @@
 module memory_stage ( //may be missing control signals
 		input clk,
 		input rd,
-		input alu_branch,
-		input control_branch,
+		input comp_enable,
+		input [3:0] alu_branch,
+		input [3:0] control_branch,
 		input [31:0] alu,
 		input [31:0] PC, 
 		input [31:0] dataB,
@@ -13,10 +14,12 @@ module memory_stage ( //may be missing control signals
 		output PC_mux_control,
 		output [31:0] alu_out,
 		output [31:0] data_out
-					);
+			);
 	wire [31:0] Wdata;
-					
-	branch_func m0 (.branch(alu_branch), .br_control(control_branch), .PC_mux_control(PC_mux_control));
+	
+	comparator m0 (.enable(comp_enable), .alu(alu_branch), .cond(control_branch), .br_sel(PC_mux_control));
+	
+	//branch_func m0 (.branch(alu_branch), .br_control(control_branch), .PC_mux_control(PC_mux_control));
 	
 	memory_register m1 (.clk(clk), .write(0), .read(1), .address(alu), .data(dataB), .read_data(Wdata));
 	//control signals set to constants at this point
