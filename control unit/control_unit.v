d9ddaa220259f28aea2112aaed600f8210d56bf8
CaballeroFF <CaballeroFF@users.noreply.github.com>
2017-03-16 12:37:18 -0700

    updated signals

--- control unit/control_unit.v
+++ control unit/control_unit.v
@@ -6,6 +6,7 @@ module control_unit (
 			input [4:0] controls, //[25:21]
 			input [1:0] format,   //[27:26]
 			output [7:0] signals //not actual length
+	//last 2 bits of signals will be for alu control
 					);
 	localparam [1:0]  alu = 2'b00,
 					  ls = 2'b01,
@@ -28,32 +29,32 @@ always @ *
  case(format)
 	alu: begin
 	  case(controls)
-		add: signals = set_condition ? 1 : 0; //add, not actually 1 or 0
-		addi: signals = set_condition ? 1 : 0;
-		sub: signals = set_condition ? 1 : 0; //subtract
-		subi: signals = set_condition ? 1 : 0;
-		_and: signals = set_condition ? 1 : 0; //and
-		andi: signals = set_condition ? 1 : 0;
-		_or: signals = set_condition ? 1 : 0; //or
-		ori: signals = set_condition ? 1 : 0;
-		default: signals = 0;
+		add: signals = set_condition ? 00 : 00; //add, not actually 1 or 0
+		addi: signals = set_condition ? 00 : 00;
+		sub: signals = set_condition ? 01 : 01; //subtract
+		subi: signals = set_condition ? 01 : 01;
+		_and: signals = set_condition ? 11 : 11; //and
+		andi: signals = set_condition ? 11 : 11;
+		_or: signals = set_condition ? 10 : 10; //or
+		ori: signals = set_condition ? 10 : 10;
+		default: signals = 00;
 	  endcase
 	end
 	
 	ls: begin
 	  case(controls)
-		rm: signals = set_condition ? 1 : 0; //load/store
-		imm: signals = set_condition ? 1 : 0; //immediate load/store
-		default: signals = 0;
+		rm: signals = set_condition ? 00 : 00; //load/store
+		imm: signals = set_condition ? 00 : 00; //immediate load/store
+		default: signals = 00;
 	  endcase
 	end
 	
 	br: begin
 	  case(condition)
-		default: signals = 0;
+		default: signals = 00;
 	  endcase
 	end
 	
-	default: signals = 0;
+	default: signals = 00;
  endcase
 endmodule
