e42ebc41e4585baf8c8f025d2b42e961e1645d49
CaballeroFF <CaballeroFF@users.noreply.github.com>
2017-03-16 11:49:47 -0700

    added enable

--- MemoryStage/comparator.v
+++ MemoryStage/comparator.v
@@ -1,11 +1,12 @@
 `timescale 1ns / 1ps
 
 module br_comp(
+      input enable,
       input [3:0] alu,
       input [3:0] cond,
       output reg br_sel
         );
-always @ *
+      always @ (posedge enable)
   br_sel = (alu == cond) ? 1 : (cond == 1110) ? 1 : 0;
         
 endmodule
