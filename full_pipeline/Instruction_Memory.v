1f90651334e04914669a4dfc0935af6c02e1468b
CaballeroFF <CaballeroFF@users.noreply.github.com>
2017-03-23 21:05:57 -0700

    Update Instruction_Memory.v

--- full_pipeline/Instruction_Memory.v
+++ full_pipeline/Instruction_Memory.v
@@ -18,17 +18,29 @@ module Instruction_Memory(
 	 reg [31:0] memory [255:0]; //for word addressable
 	 //and we would need to initialize each memory index as one 16'b word
 	 initial begin
-		memory[0] = 32'h00801002; //r1 = r0+r2
+	    memory[0] = 32'h813002;    //r3 = r1+r2  testing forwarding
+		memory[1] = 32'h834001;    //r4 = r3+r1
+		memory[2] = 32'h834001;    //r4 = r3+r1
+		memory[3] = 32'h834001;    //r4 = r3+r1
+		memory[4] = 32'h834001;    //r4 = r3+r1
+		memory[5] = 32'h834001;    //r4 = r3+r1
+
+	  /* memory[0] = 32'h07000000;  //store r0 to 0x0000  testing load/store
+		memory[1] = 32'h00801003;   //some add func
+		memory[2] = 32'h00801003;
+		memory[3] = 32'h00801003; 
+		memory[4] = 32'h00801003;
+		memory[5] = 32'h51F1000;    //loading previously stored reg
+		memory[6] = 32'h51F0000;
+		memory[7] = 32'h71F0000;
+		memory[8] = 32'h7100000;
+		memory[8] = 32'h7100009;*/
+		/*memory[0] = 32'h00801002; //r1 = r0+r2  testing branch 
 		memory[1] = 32'h00801003; //r1 = r0+r3
-		memory[2] = 32'hEAFFFFFD;
+		memory[2] = 32'hEAFFFFFD; //unconditional branch to pc - 3
 		memory[3] = 32'h00401003; //r1 = r0-r3
 		memory[4] = 32'h51A6003;
-		memory[5] = 32'h50A6003;
-		/*memory[5] = 32'hEAFFFFFE;
-		memory[6] = 8'h18;
-		memory[7] = 8'h18;
-		memory[8] = 8'hD1;
-		memory[9] = 8'h18;*/
+		memory[5] = 32'h50A6003;*/
 	 end
 	 
 	 always @ (posedge clk)
