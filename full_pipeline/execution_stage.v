918211312237e39f34c88ed25aebaa2999e7ffb6
CaballeroFF <CaballeroFF@users.noreply.github.com>
2017-03-23 21:00:33 -0700

    Update execution_stage.v

--- full_pipeline/execution_stage.v
+++ full_pipeline/execution_stage.v
@@ -1,8 +1,10 @@
 `timescale 1ns / 1ps
 
 module ex_stage (
-		input clk,
-		//input set_cond,
+		input clk, reset,
+		input wb_ex, wb_mem,
+		input [3:0] rn, rm, rd_ex, rd_mem,
+		input [31:0] ex_data, mem_data,
 		input [3:0] br_cond,
 		input [3:0] alu_cond,  //comes from mem stage
 		input [31:0] alu_imm,  //comes from decode
@@ -21,9 +23,11 @@ module ex_stage (
 		output [3:0] br_cond_out,
 		output [10:0] sign_out
 				);
-	wire [31:0] WPC, Walu, Wres, Wimm;
+				
+	wire [31:0] WPC, Walu, Wres, Wimm, Wm1, Wm2;
 	wire [3:0] Wcond;	           //from reserve to latch
 	wire [3:0] Wbr;
+	wire [1:0] sel1, sel2;
 	
 	branch_adder e0(.PC(PC), .offset(br_imm), .newPC(WPC));
 
@@ -32,12 +36,18 @@ module ex_stage (
 	im_mux e2 (.mem_reg(dataB), .immediate(Wimm), .select(signals[4]), .out(Walu));
 	//select comes from decode stage control unit
 	
-	alu e3 (.A(dataA), .B(Walu), .control(signals[2:0]), .branch(Wbr), .result(Wres));
+	forwarding e3(.A(rn), .B(rm), .ex_rd(rd_ex), .mem_rd(rd_mem), .ctrl_ex(wb_ex), .ctrl_mem(wb_mem), .A_sel(sel1), .B_sel(sel2));
+	
+	FourToOne_Mux e4(.data(dataA), .ex(ex_data), .mem(mem_data), .sel(sel1), .out(Wm1));
+	
+	FourToOne_Mux e5(.data(Walu), .ex(ex_data), .mem(mem_data), .sel(sel2), .out(Wm2));
+	
+	alu e6 (.A(Wm1), .B(Wm2), .control(signals[2:0]), .branch(Wbr), .result(Wres));
 	//control comes from decode stage control unit
 
-	reserve e4 (.alu_out(branch), /*.set_cond(set_cond),*/ .cond(Wcond));
+	reserve e7 (.alu_out(branch), .cond(Wcond));
 	
-	ex_latch e5 (.clk(clk), .br_cond(br_cond), .br(Wbr), .alu_cond(Wcond), .alu(Wres), .adder(WPC), .writedata(dataA), 
+	ex_latch e8 (.clk(clk), .reset(reset), .br_cond(br_cond), .br(Wbr), .alu_cond(Wcond), .alu(Wres), .adder(WPC), .writedata(Wm1/*dataA*/), 
 		     .rd(rd),.br_out(branch), .alu_out(alu_result), .adder_out(newPC), .write_out(write_out),
 		     .rd_out(rd_out), .alu_cond_out(alu_cond_out), .br_cond_out(br_cond_out), .signals(signals), .sign_out(sign_out));
 endmodule
