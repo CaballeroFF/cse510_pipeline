4cbf4517727b9db44702dcf397129a27c6d7035c
CaballeroFF <CaballeroFF@users.noreply.github.com>
2017-03-23 20:57:55 -0700

    Update decode_latch.v

--- full_pipeline/decode_latch.v
+++ full_pipeline/decode_latch.v
@@ -11,12 +11,13 @@
 module decode_latch(
 //input control signals in the future
 			input clk,
+			input reset,
 			input [31:0] next_pc,
 			input [31:0] dataA, dataB,
 			input [31:0] br_se,             //branch sign extended   
 			input [31:0] ls_se,             //load / store sign extended
 			input [31:0] alu_se,            // alu sign extended
-			input [3:0]  rd,
+			input [3:0]  rd, rn, rm,
 			input [10:0] signals,
 			input [3:0] instr_cond,
 			//input [4:0]  rd, for immediate type
@@ -25,12 +26,27 @@ module decode_latch(
 			output reg [31:0] br_se_out,
 			output reg [31:0] ls_se_out,
 			output reg [31:0] alu_se_out,
-			output reg [3:0] rd_out,
+			output reg [3:0] rd_out, rn_out, rm_out,
 			output reg [10:0] sign_out,
 			output reg [3:0] br_cond
 					);
 					
 		always @ (posedge clk)
+		if (reset)
+		begin
+		pc_out <= 0;
+		dataA_out <= 0;
+		dataB_out <= 0;
+		br_se_out <= 0;
+		ls_se_out <= 0;
+		alu_se_out <= 0;
+		rd_out <= 0;
+		rn_out <= 0;
+		rm_out <= 0;
+		sign_out <= 0;
+		br_cond <= 0;
+		end
+		else
 		begin
 		pc_out <= next_pc;
 		dataA_out <= dataA;
@@ -39,6 +55,8 @@ module decode_latch(
 		ls_se_out <= ls_se;
 		alu_se_out <= alu_se;
 		rd_out <= rd;
+		rn_out <= rn;
+		rm_out <= rm;
 		sign_out <= signals;
 		br_cond <= instr_cond;
 		end
